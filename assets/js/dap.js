53feef936543c0865bb1fa88d2493b42f7a29251
Ohunayo Gege <ohunayogege@gmail.com>
2021-07-15 02:19:57 +0100

    add slider

--- assets/js/dap.js
+++ assets/js/dap.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
   $('.nav-button').click(function(){
 	$('body').toggleClass('nav-open');
   });
+  $("#learn-dap").click(function(){
+    $("#sub-learn").toggleClass("list-none");
+  });
 });
